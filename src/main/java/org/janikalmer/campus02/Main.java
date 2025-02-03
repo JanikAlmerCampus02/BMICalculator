453af3817c5d812fe033053fdd67265c48c10701
JanikAlmerCampus02 <12325225@edu.campus02.at>
2025-02-03 19:39:22 +0100

    BMICalculatorConstructorTest

--- src/main/java/org/janikalmer/campus02/Main.java
+++ src/main/java/org/janikalmer/campus02/Main.java
@@ -1,17 +1,23 @@
 package org.janikalmer.campus02;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Beispielinstanzen für den BMICalculator
+        BMICalculator person1 = new BMICalculator("Max", "Mustermann", 170, 70, 'm');
+        BMICalculator person2 = new BMICalculator("Lisa", "Müller", 180, 120, 'w');
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // BMI und Kategorien berechnen
+        printBMI(person1);
+        printBMI(person2);
+    }
+
+    public static void printBMI(BMICalculator person) {
+        System.out.println("------------------------------------------------");
+        System.out.println("Name: " + person.getFirstname() + " " + person.getLastname());
+        System.out.println("Größe: " + person.getBodyHeight() + " cm");
+        System.out.println("Gewicht: " + person.getBodyWeight() + " kg");
+        System.out.println("BMI: " + person.calculateBMI());
+        System.out.println("Kategorie: " + person.getBMICategoryName());
+        System.out.println("------------------------------------------------");
     }
 }
--- src/test/java/org/janikalmer/campus02/BMICalculatorTest.java
+++ src/test/java/org/janikalmer/campus02/BMICalculatorTest.java
@@ -6,26 +6,11 @@ import org.junit.jupiter.api.Test;
         @Test
         public void testConstructor() {
             BMICalculator bmi = new BMICalculator("Max", "Mustermann", 170, 70, 'm');
+            assertEquals("Max", bmi.getFirstname());
+            assertEquals("Mustermann", bmi.getLastname());
             assertEquals(170, bmi.getBodyHeight());
             assertEquals(70.0, bmi.getBodyWeight());
-        }
-
-        @Test
-        public void testCalculateBMI() {
-            BMICalculator bmi = new BMICalculator("Lisa", "Müller", 180, 120, 'w');
-            assertEquals(37.04, bmi.calculateBMI(), 0.01);
-        }
-
-        @Test
-        public void testCalculateBMICategory() {
-            BMICalculator bmi = new BMICalculator("John", "Doe", 170, 70, 'm');
-            assertEquals(0, bmi.calculateBMICategory());
-        }
-
-        @Test
-        public void testGetBMICategoryName() {
-            BMICalculator bmi = new BMICalculator("Jane", "Doe", 160, 80, 'w');
-            assertEquals("Übergewicht", bmi.getBMICategoryName());
+            assertEquals('m', bmi.getGender());
         }
     }
 
